a3af4e9b5a490f09ea4d9aa393c8b3b8b295bbc5
sheldon <sheldonmwangi254@gmail.com>
2023-08-07 09:54:05 +0300

    completed assignment

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,4 +1,36 @@
-// components/BotCollection.js
+// // components/BotCollection.js
+// import React, { useState, useEffect } from "react";
+// import { fetchBots } from "../services/api";
+
+// const BotCollection = ({ addToBotArmy }) => {
+//   const [bots, setBots] = useState([]);
+
+//   useEffect(() => {
+    
+//     const getBots = async () => {
+//       const botsData = await fetchBots();
+//       setBots(botsData);
+//     };
+//     getBots();
+//   }, []);
+
+//   return (
+//     <div>
+//       <h2>Bot Collection</h2>
+//       {/* Render the list of bots */}
+//       {bots.map((bot) => (
+//         <div key={bot.id}>
+//           <img src={bot.avatar_url} alt={bot.name} />
+//           <h3>{bot.name}</h3>
+//           {/* Add bot to the army */}
+//           <button onClick={() => addToBotArmy(bot)}>Enlist</button>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default BotCollection;
 import React, { useState, useEffect } from "react";
 import { fetchBots } from "../services/api";
 
@@ -6,26 +38,26 @@ const BotCollection = ({ addToBotArmy }) => {
   const [bots, setBots] = useState([]);
 
   useEffect(() => {
-    // Fetch bots data from the server and update the state
-    const getBots = async () => {
-      const botsData = await fetchBots();
-      setBots(botsData);
-    };
-    getBots();
+    fetchData();
   }, []);
+
+  const fetchData = async () => {
+    const botsData = await fetchBots();
+    setBots(botsData);
+  };
+
+  const renderBot = (bot) => (
+    <div key={bot.id}>
+      <img src={bot.avatar_url} alt={bot.name} />
+      <h3>{bot.name}</h3>
+      <button onClick={() => addToBotArmy(bot)}>Enlist</button>
+    </div>
+  );
 
   return (
     <div>
       <h2>Bot Collection</h2>
-      {/* Render the list of bots */}
-      {bots.map((bot) => (
-        <div key={bot.id}>
-          <img src={bot.avatar_url} alt={bot.name} />
-          <h3>{bot.name}</h3>
-          {/* Add bot to the army */}
-          <button onClick={() => addToBotArmy(bot)}>Enlist</button>
-        </div>
-      ))}
+      {bots.map(renderBot)}
     </div>
   );
 };
